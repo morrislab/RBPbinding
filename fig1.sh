e0b18c316b4ec4175c18214f37f9e9f318fb36a2
LXsasse <alexander.sasse@gmail.com>
2023-06-04 21:04:54 -0700

    Add files via upload

--- fig1.sh
+++ fig1.sh
@@ -32,20 +32,22 @@ mpearson=${outdir}${zscores%.txt}'_pearson.mat'
 
 
 
-# piechart figure 1
-python Scripts/piechart.py ${masterfile} ${fig1}
 
 ### Figure 1A # Motif similarity matrix after sequence identity clustering
 python Scripts/make_newick_phylo.motifs.3.py $seqid single None 'Seq identity' 100.,50.,0. ${masterfile} ${outdir}${zscores%.txt}_pwm.hmot --savefig $fig1 150 --showsimilarity $mpearson --markset 291 --assignspecies --proteinlist ${indir}${highestzscoreset}
 
-### Figure 1B # Motif similarity matrix after sequence identity clustering for a-rich cluster
-python Scripts/make_newick_phylo.motifs.3.py $seqid single None 'Seq identity' 100.,50.,0. $masterfile ${outdir}${zscores%.txt}_pwm.hmot --savefig $fig1 150 --showsimilarity $mpearson --markset 291 --assignspecies --proteinlist ${indir}pearsonlist-arich.txt
+#### Figure 1B: # measured RBPs
+python Scripts/piechart.py ${masterfile} ${supfig}
 
-# Figure 1C # Individual z-score scatter plots for sart3 family
-python Scripts/make_newick_phylo.motifs.3.py $seqid single 30 'Seq identity' 100.,50.,0. $masterfile ${outdir}${zscores%.txt}_pwm.hmot --savefig ${fig1} 200 --showindividual ${indir}${zscores} --proteinlist ${indir}pearsonlist-sart3.list
+### Figure 1C # Number of bound 7-mers in each kingdom
+python3 Scripts/bound_kmers.py Data/Zscores_420_origrncmpt.txt Data/RBP_420domainclass.txt Figures/Figure1/7mernumbers_perdomain.jpg
 
 # Figure 1D Sequence identity to Pearson correlation relationship
 python Scripts/pearson_vs_id.py 1 $mpearson $seqid --protlist ${indir}${rrmkhlist} --boxplot --ylabel 'Pearson' --selfcorr ${indir}Fullset_z_scores-setA-vs-setB-comparison.txt 1 --setylim 0 1 --outname $fig1
+
+### Figure 1E # Motif similarity matrix after sequence identity clustering for a-rich cluster
+python Scripts/make_newick_phylo.motifs.3.py $seqid single None 'Seq identity' 100.,50.,0. $masterfile ${outdir}${zscores%.txt}_pwm.hmot --savefig $fig1 150 --showsimilarity $mpearson --markset 291 --assignspecies --proteinlist ${indir}pearsonlist-arich.txt
+
 
 
 
@@ -69,5 +71,10 @@ python Scripts/make_newick_phylo.motifs.3.py $mpearson complete None 'Pearson' 1
 python Scripts/pearson_vs_id.py 1 $mpearson $seqid --protlist ${indir}RNCMPT_unique_experiments_KH.list --boxplot --ylabel 'Pearson' --selfcorr ${indir}Fullset_z_scores-setA-vs-setB-comparison.txt 1 --setylim 0 1 --outname $supfig
 python Scripts/pearson_vs_id.py 1 $mpearson $seqid --protlist ${indir}RNCMPT_unique_experiments_RRM.list --boxplot --ylabel 'Pearson' --selfcorr ${indir}Fullset_z_scores-setA-vs-setB-comparison.txt 1 --setylim 0 1 --outname $supfig
 python Scripts/pearson_vs_id.py 1 ${outdir}${zscores%.txt}_top100.mat $seqid --protlist ${indir}RNCMPT_unique_experiments_RRM,KH.list --boxplot --ylabel 'Pearson' --selfcorr ${indir}Fullset_z_scores-setA-vs-setB-comparison.txt 3 --setylim 0 1 --outname $supfig
+
+
+# Individual z-score scatter plots for sart3 family
+python Scripts/make_newick_phylo.motifs.3.py $seqid single 30 'Seq identity' 100.,50.,0. $masterfile ${outdir}${zscores%.txt}_pwm.hmot --savefig ${supfig} 200 --showindividual ${indir}${zscores} --proteinlist ${indir}pearsonlist-sart3.list
+
 
 
